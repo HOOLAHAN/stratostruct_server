ead9b46151172e4652e5015fc0837fedc70245b6
Iain <iain.hoolahan@gmail.com>
2023-03-30 14:24:03 +0100

    set up mongodb and mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const supplierRoutes = require('./routes/suppliers')
+const mongoose = require('mongoose')
 
 // express app
 const app = express()
@@ -16,8 +17,17 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/suppliers', supplierRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+// connect to DB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to DB & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
 
