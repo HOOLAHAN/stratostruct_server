8beca36672020ec796810ab632e61c37d434295c
Iain <iain.hoolahan@gmail.com>
2023-04-05 14:38:17 +0100

    validation added to email and password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -25,6 +26,17 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function(email, company, full_name, password) {
+
+  // validation
+  if (!email || !companny || full_name || !password) {
+    throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email is not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
 
   const exists = await this.findOne({ email })
 
