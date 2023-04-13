5dddb5a78ed8edda6202d7a77483a27f3223fb8c
Iain <iain.hoolahan@gmail.com>
2023-04-13 11:52:26 +0100

    mapbox api route added to server

--- server.js
+++ server.js
@@ -21,6 +21,41 @@ app.use('/api/suppliers', supplierRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/user', userRoutes)
 
+// route handler for the Mapbox API endpoint
+app.get('/api/distance', async (req, res) => {
+  try {
+    const { postcode1, postcode2 } = req.query;
+
+    // get the latitude and longitude coordinates for each postcode
+    const geocodingUrl1 = `https://api.mapbox.com/geocoding/v5/mapbox.places/${postcode1}.json?access_token=${process.env.MAPBOX_API_KEY}`;
+    const geocodingUrl2 = `https://api.mapbox.com/geocoding/v5/mapbox.places/${postcode2}.json?access_token=${process.env.MAPBOX_API_KEY}`;
+
+    const [data1, data2] = await Promise.all([
+      fetch(geocodingUrl1).then(response => response.json()),
+      fetch(geocodingUrl2).then(response => response.json())
+    ]);
+
+    const coordinates1 = data1.features[0].center;
+    const coordinates2 = data2.features[0].center;
+
+    // calculate the driving distance between the two points
+    const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${coordinates1[0]},${coordinates1[1]};${coordinates2[0]},${coordinates2[1]}?access_token=${process.env.MAPBOX_API_KEY}`;
+
+    const drivingDistance = await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const distanceInMeters = data.routes[0].distance;
+        const distanceInKilometers = (distanceInMeters / 1000).toFixed(2);
+        return distanceInKilometers;
+      });
+
+    res.json({ distance: drivingDistance });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
 // connect to DB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
@@ -32,6 +67,3 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((error) => {
     console.log(error)
   })
-
-
-
